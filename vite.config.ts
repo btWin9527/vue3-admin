824197384ec916db60713e94b4bb71de7fc6d7a0
郭吉光 <41253028+btWin9527@users.noreply.github.com>
2023-02-01 10:50:47 +0800

    feat: modify resource

--- vite.config.ts
+++ vite.config.ts
@@ -111,21 +111,21 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
         }
       }
     },
-    server: {
-      port: 4000,
-      proxy: {
-        // 选项写法
-        '/api': {
-          target: 'http://127.0.0.1:8000',
-          changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, '')
-        }
-      },
-      hmr: {
-        overlay: false
-      },
-      host: '0.0.0.0'
-    },
+    // server: {
+    //   port: 4000,
+    //   proxy: {
+    //     // 选项写法
+    //     '/api': {
+    //       target: 'http://127.0.0.1:8000',
+    //       changeOrigin: true,
+    //       rewrite: path => path.replace(/^\/api/, '')
+    //     }
+    //   },
+    //   hmr: {
+    //     overlay: false
+    //   },
+    //   host: '0.0.0.0'
+    // },
     optimizeDeps: {
       include: [
         'vue',
