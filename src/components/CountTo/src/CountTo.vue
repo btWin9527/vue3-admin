7b058519ba3047fa4a43981ca41761c9307e7302
郭吉光 <1135899486@qq.com>
2024-04-12 17:23:23 +0800

    feat: 添加二维码信息描述

--- src/components/CountTo/src/CountTo.vue
+++ src/components/CountTo/src/CountTo.vue
@@ -1,3 +1,20 @@
+<!--
+  CountTo 数字动画组件
+
+  CountTo 属性
+  属性	说明	类型	可选值	默认值
+  startVal	初始值	number	-	0
+  endVal	最后展示的值	number	-	2021
+  duration	动画时间	number	-	3000
+  autoplay	是否自动播放	boolean	-	true
+  decimals	小位数	number	-	0
+  decimal	小位数分割符号	string	-	.
+  separator	分割符号	string	-	,
+  prefix	前缀	string	-	-
+  suffix	后缀	string	-	-
+  useEasing	过渡动画	boolean	-	true
+  easingFn	自定义动画效果	(t: number, b: number, c: number, d: number) => number	-	-
+-->
 <script setup lang="ts">
 import { reactive, computed, watch, onMounted, unref, toRef, PropType } from 'vue'
 import { isNumber } from '@/utils/is'
--- src/components/Qrcode/src/Qrcode.vue
+++ src/components/Qrcode/src/Qrcode.vue
@@ -1,3 +1,22 @@
+<!--
+ Qrcode 二维码组件
+
+ Qrcode 属性
+  属性	说明	类型	可选值	默认值
+  tag	以什么标签生成二维码	string	canvas/img	canvas
+  text	二维码内容	string/Array	-	-
+  options	qrcode.js 配置项	QRCodeRenderersOptions	-	{}
+  width	二维码宽度	number	-	200
+  logo	二维码 logo	QrcodeLogo/string	-	-
+  disabled	二维码是否过期	boolean	-	false
+  disabledText	二维码过期提示内容	string	-	-
+
+  Qrcode 事件
+  方法名	说明	回调参数
+  done	生成二维码后的回调	-
+  click	二维码点击事件	-
+  disabled-click	二维码过期后点击事件	-
+ -->
 <script setup lang="ts">
 import { PropType, nextTick, ref, watch, computed, unref } from 'vue'
 import QRCode from 'qrcode'
